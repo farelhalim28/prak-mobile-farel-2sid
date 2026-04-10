13c9fbc587128ea89b2294ab69104491f7cf8e03
lab151-02 <lab151@gmail.com>
2026-04-10 10:34:55 +0700

    commit late two

--- app/src/main/java/com/example/cobaapps/Pertemuan_3/ThirdActivity.kt
+++ app/src/main/java/com/example/cobaapps/Pertemuan_3/ThirdActivity.kt
@@ -1,5 +1,6 @@
 package com.example.cobaapps.Pertemuan_3
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
@@ -31,6 +32,9 @@ class ThirdActivity : AppCompatActivity() {
 
             Toast.makeText(
                 this, "Pesan berhasil dikirim ke $noTujuan", Toast.LENGTH_SHORT).show()
+
+            val intent = Intent(this, ThirdResultActivity::class.java)
+            startActivity(intent)
         }
     }
 }
