69f11fc5f96f24b7f0531790556810b1ff9b671e
lab151-02 <lab151@gmail.com>
2026-04-17 09:52:44 +0700

    commit pertemuan 4

--- app/src/main/java/com/example/cobaapps/MainActivity.kt
+++ app/src/main/java/com/example/cobaapps/MainActivity.kt
@@ -1,20 +1,43 @@
 package com.example.cobaapps
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.cobaapps.Pertemuan_4.FourthActivity
+import com.example.cobaapps.databinding.ActivityFourthBinding
+import com.example.cobaapps.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        binding.btnKeempat.setOnClickListener {
+
+//            /tambahkan bagian berikut/
+
+            intent.putExtra("nama", "Politeknik Caltex Riau")
+            intent.putExtra("asal", "Rumbai")
+            intent.putExtra("usia", 25)
+
+            startActivity(intent)
+            val intent = Intent(this, FourthActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
+
